fff058748892793dece97430355ffdfd94f9af9a
MESS Team <contato@mess.com.br>
2025-12-22 13:09:47 -0300

    feat(api): start workers in api process

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -3,6 +3,8 @@ import { config } from '../shared/env.js'
 import { logger } from '../shared/logger.js'
 import { closePrisma, getPrisma } from '../shared/prismaClient.js'
 import { closeRedis, getRedis } from '../shared/redis.js'
+import { startAutomationWorker } from '../worker/automationProcessor.js'
+import { startOutboxWorker } from '../worker/outboxProcessor.js'
 
 const port = config.PORT || 3000
 
@@ -10,9 +12,14 @@ const server = app.listen(port, () => {
   logger.info({ port }, 'API iniciada')
 })
 
+const automationWorker = startAutomationWorker()
+const outboxWorker = startOutboxWorker()
+
 const shutdown = async () => {
   logger.info({}, 'Encerrando servidor...')
   server.close()
+  await automationWorker.stop()
+  await outboxWorker.stop()
   await closePrisma()
   await closeRedis()
   process.exit(0)
